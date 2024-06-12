38caf59eba2951ab4c9446e6b0cf5562e8203a43
bisht65190 <bisht65190@gmail.com>
2024-06-12 15:31:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,262 @@
+"use strict"
+// console.log("hello world");
+
+// x=78
+// console.log("value of x is",x);
+// console.log(x);
+
+// {
+//    let a=10
+//    console.log(a); 
+// }
+// console.log(a); 
+
+// {
+//   const b=13
+//     console.log(b); 
+//  }
+//  console.log(b); 
+
+// {
+//     var c=67
+//     console.log(c);
+// }
+// console.log(c);
+
+
+
+// data types 
+
+// number 
+
+let number=45
+// console.log(typeof number);
+
+// string 
+// let str=`hello`;
+// console.log(typeof str);
+
+
+
+// boolean ----- true/false 
+
+let isPresent=true
+// console.log(typeof isPresent);
+
+// null 
+
+let num2=null
+// console.log(typeof num2);
+
+let num3;
+// console.log(num3);
+// console.log(typeof num3);
+
+
+// big int 
+let bigNumber=BigInt(98879634645768769)
+// console.log(bigNumber);
+
+
+// symbol 
+
+// let symbol1=Symbol("symbol1")
+// let symbol2=Symbol("symbol1")
+
+
+
+// let a=45
+// let b=a
+// console.log("a is ",a);
+// console.log("b is ",b);
+// a=78
+// console.log("a is ",a);
+// console.log("b is ",b);
+
+
+// type conversion 
+
+// number to string 
+// let num=String(67)
+// or 
+// let num=67+" "
+// console.log(typeof num);
+
+
+// string to number 
+
+// let str=Number("67")
+// or 
+// let str=+"67"
+// console.log(typeof str);
+
+// let userInput=+prompt("enter your number")
+// console.log("type of user input",typeof userInput);
+
+
+// let a="3"
+// let b=6
+// let res=a+b
+// console.log(a);
+
+// let a="6"
+// let b=3
+// let res=a-b
+// console.log(res);
+
+
+// let a="3"
+// let b=6
+// let res=a*b
+// console.log(res);
+
+// let a="3"
+// let b=6
+// let res=a/b
+// console.log(res);
+
+
+// let a="3"
+// let b=5
+// console.log(a!==b);
+// console.log(a!=b);
+
+// let str="sonal"
+// console.log(str.length);
+// console.log(str[1]);
+
+// let a=null
+// let b
+// console.log(b);
+// console.log(a==b);
+
+// let res=null
+
+
+// arithmatic operators 
+
+// let a=10;
+// let b=20;
+// let res=a+b
+// console.log(res);
+
+
+// let a=10;
+// let b=20;
+// let res=a-b
+// console.log(res);
+
+
+// let a=10;
+// let b=20;
+// let res=a*b
+// console.log(res);
+
+// let a=10;
+// let b=20;
+// let res=a/b
+// console.log(res);
+
+
+// let a=10;
+// let b=20;
+// let res=a%b
+// console.log(res);
+
+
+// let a=10
+// console.log(a);
+// a++
+// console.log(a);
+// a--
+// console.log(a);
+
+// a+=10
+// console.log(a);
+
+// a-=10
+// console.log(a);
+
+// a*=10
+// console.log(a);
+
+
+// a/=10
+// console.log(a);
+
+// a%=10
+// console.log(a);
+
+
+
+// let a=10
+// let b="10"
+// console.log(a==b);
+// console.log(a===b);
+
+// console.log(a!=b);
+// console.log(a!==b);
+
+
+// let isBool=false
+// let str=null
+// console.log(isBool==str);
+// 0==false
+
+
+// console.log(Number(str));
+// console.log(typeof Number(str));
+
+
+// console.log(+null);
+// console.log(+undefined);
+
+
+
+// logical operators
+// && || !
+
+let a=10
+let b=20
+
+let result=!!(a>10)&&(b<25)
+// let result=(a>10)||(b<25)
+
+
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
